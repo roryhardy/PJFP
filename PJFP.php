a43c53063045fc838b1add2a1d792baff7987f1a
GneatGeek <gneatgeek@gmail.com>
2012-06-27 23:02:11 -0700

    More updates - not tested

--- PJFP.php
+++ PJFP.php
@@ -6,7 +6,7 @@
  * @copyright (c) 2012, Rory Hardy [GneatGeek]
  * @license http://www.opensource.org/licenses/BSD-3-Clause BSD-3 Clause.  The license is included in the repo.
  * @link http://github.com/gneatgeek/PJFP
- * @version 1.4.3
+ * @version 1.4.5
  */
 
 // --------------------------------------------------------------------------------------
@@ -19,9 +19,9 @@
  */
 class PJFP {
 	/**
-	 * The name and absolute path of the config file to use. This shouldn't need to be changed.
+	 * The name and/or path of the config file to use. This shouldn't need to be changed.
 	 * If the file is in the same dir as PJFP the path may be omitted!
-	 * If it does, do it here!
+	 * If it needs to be changed, do it here!
 	 * @var string
 	 */
 	private $config_file = "PJFP_config.php";
@@ -71,7 +71,6 @@ class PJFP {
 
 	/**
 	 * Constructor
-	 * @access public
 	 * @param string $albumID - Picasa RSS album ID.
 	 * @param array $conf - Associative array used to override default settings in config.inc.  See PJFP_config.php for parameters
 	 * @param string $authKey - Key that picasa uses for limited/private galleries.
@@ -84,9 +83,8 @@ class PJFP {
 	}
 
 	/**
-	 * Set a config variable
-	 * @access public
-	 * @param string $var - The key of the config array to set
+	 * Sets a config variable
+	 * @param string $key - The key of the config array to set
 	 * @param mixed $value - The config value to use
 	 */
 	final public function set_conf($key, $value) {
@@ -95,8 +93,8 @@ class PJFP {
 
 	/**
 	 * Build the internal config array based off of the config file and user defined parameters
-	 * @access private
 	 * @param array $conf - The passed in array from the user to the constructor.
+	 * @throws Exception - Config File is MIA
 	 */
 	final private function build_conf(&$conf) {
 		include ($this -> config_file);
@@ -112,23 +110,24 @@ class PJFP {
 	}
 
 	/**
-	 * Method to retrieve the 2D array of data created by json()
+	 * Retrieve the 2D array of data created by json()
 	 * Format is array(URL, width, height, caption)
+	 *   Default is numeric indicies.
 	 * Calls json() if data array is not yet built.
-	 * @param int $type - What type of array to request. PJFP::ASSOC for associative, PJFP::BOTH for mixed, and NULL for numeric.
+	 * @param int $type - What type of array to request. PJFP::ASSOC for associative, PJFP::BOTH for mixed
 	 * @return array
 	 */
 	public function get_data($type = self::NUMERIC) {
 		if (empty($this -> data))
 			$this -> json();
 
-		# Needs to be before the numeric case and after json call.
+		# Needs to be before the numeric case and after the json call.
 		$tmp_array = array_fill(0, count($this -> data), array());
 
-		if (($type & 0x01) != 0)# Numeric case
+		if (($type & 0x01) != 0) # Numeric case
 			$tmp_array = $this -> data;
 
-		if (($type & 0x02) != 0) {# Associative case
+		if (($type & 0x02) != 0) { # Associative case
 			static $keys = array("URL", "width", "height", "caption");
 
 			foreach ($this->data as $key => &$node)
@@ -140,7 +139,6 @@ class PJFP {
 
 	/**
 	 * Basic curl method to get data from Picasa
-	 * @access private
 	 * @throws Exception - curl failed.
 	 * @return string
 	 */
@@ -168,7 +166,6 @@ class PJFP {
 	/**
 	 * This is simply an alternative to curl since some systems may not have it available.
 	 * Both methods ultimately do the same thing.
-	 * @access private
 	 * @throws Exception - socket failed
 	 * @return string
 	 */
@@ -208,7 +205,6 @@ class PJFP {
 	/**
 	 * Method to parse the JSON data fetched from Picasa
 	 * Sets:  $this->data to decoded JSON as an array (NOT AN OBJECT)
-	 * @access private
 	 */
 	private function json() {
 		$json = ($this -> config['use_curl'] ? $this -> curl() : $this -> socket());
@@ -237,20 +233,17 @@ class PJFP {
 	}
 
 	/**
-	 * Image Resize Method
 	 * Adjusts the vertical image's width x height parameters so it fits the bounds of the gallery dynamically.
 	 * If the image is horizontal (width > height) then Picasa will spit it out at the correct size already
 	 *   so there is no reason to resize those images.
-	 * @access private
-	 * @param $width - Variable reference to width
-	 * @param $height - Variable reference to height
+	 * @param int $width - Variable reference to width
+	 * @param int $height - Variable reference to height
 	 */
 	private function image_resize(&$width, &$height) {
 		if ($height > $this -> config['max_height']) {
-			if ($height == $width) {
-				$width  = $this -> config['max_height'];
-				$height = $this -> config['max_height'];
-			} else {
+			if ($height == $width)
+				$width  = $height = $this -> config['max_height'];
+			else {
 				$width  = round($width * ($this -> config['max_height'] / $height));
 				$height = $this -> config['max_height'];
 			}
@@ -259,11 +252,11 @@ class PJFP {
 
 	/**
 	 * Getter method to access the config array.
-	 * @access public
-	 * @param $index - The array index to return.
+	 * @param string $index - The array index to return.
 	 * @return mixed
 	 */
 	public function get_config($index) {
+			
 		return ($this -> config[$index]);
 	}
 }
